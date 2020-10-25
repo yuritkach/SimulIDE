c778baaec0490716cdda0778d5be6c08d97456b2
yuritkach <yuritkach@gmail.com>
2020-10-25 22:37:57 +0200

    zxcZX

--- SimulIDE/src/gui/editor/BaseDebugger.cs
+++ SimulIDE/src/gui/editor/BaseDebugger.cs
@@ -1,9 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Forms;
+using TextBox = System.Windows.Controls.TextBox;
+
+//#include "basedebugger.h"
+//#include "baseprocessor.h"
+//#include "editorwindow.h"
+//#include "mainwindow.h"
+//#include "simulator.h"
+
+
 
 namespace SimulIDE.src.gui.editor
 {
@@ -20,6 +31,7 @@ namespace SimulIDE.src.gui.editor
 
             processorType = 0;
             type = 0;
+            loadStatus = false;
 
             //connect(&m_compProcess, SIGNAL(readyRead()), SLOT(ProcRead()));
 
@@ -40,33 +52,146 @@ namespace SimulIDE.src.gui.editor
         protected virtual void getSubs() {; }
 
         protected string compilerPath;
-        public string CompilerPath
+        public string CompilerPath{ get { return compilerPath; } }
+
+        public void SetCompilerPath()
         {
-            get
+            using (var fbd = new FolderBrowserDialog())
             {
-                return compilerPath;
-            }
-            set
-            {
-                compilerPath = value;
-                compilerPath = QFileDialog::getExistingDirectory(0L,
-                           tr("Select Compiler toolchain directory"),
-                           m_compilerPath,
-                           QFileDialog::ShowDirsOnly
-                         | QFileDialog::DontResolveSymlinks);
+                DialogResult result = fbd.ShowDialog();
 
-                if (m_compilerPath != "") m_compilerPath += "/";
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    compilerPath = fbd.SelectedPath;
+                    //MainWindow::self()->settings()->setValue(m_compSetting, m_compilerPath);
 
-                MainWindow::self()->settings()->setValue(m_compSetting, m_compilerPath);
-
-                m_outPane->appendText("\n" + tr("Using Compiler Path: ") + "\n");
-                m_outPane->writeText(m_compilerPath + "\n\n");
-
+                    outPane.AppendText("\nUsing Compiler Path: \n");
+                    outPane.AppendText(compilerPath + "\n\n");
+                }
+                else compilerPath = "";
             }
         }
+
+        protected bool LoadFirmware()
+        {
+            if (firmware == "") return false;
+
+            Upload();
+            if (loadStatus) return false;
+
+            loadStatus = true;
+            return true;
+        }
+
+        public void Upload()
+        {
+            if (loadStatus)
+            {
+                MessageBox.Show("Debugger already running\nStop active session");
+                return;
+            }
+            outPane.AppendText("-------------------------------------------------------\n");
+            outPane.AppendText("\nUploading: \n");
+            outPane.AppendText(firmware);
+            outPane.AppendText("\n\n");
+
+//            if (McuComponent.Self())
+//            {
+//                McuComponent.Self().Load(firmware);
+//                outPane.AppendText("\nFirmWare Uploaded to " + McuComponent.Self().device() + "\n");
+//                outPane.AppendText("\n\n");
+
+//                BaseProcessor.Self().getRamTable().SetDebugger(this);
+//                mapFlashToSource();
+//            }
+//            else outPane.AppendText("\nError: No Mcu in Simulator... \n");
+        }
+
+        public void Stop()
+        {
+            loadStatus = false;
+        }
+
+        void GetProcName(){}
+
+        int Step()
+        {
+            int pc = BaseProcessor.self().PC();
+
+            int i = 0;
+            for (i = 0; i < 10; i++) // If runs 10 times and get to same PC return 0
+            {
+                BaseProcessor.Self().StepOne();
+
+                int pc2 = BaseProcessor.self().PC();
+                //qDebug() <<"BaseDebugger::step "<<pc<<pc2;
+                if (pc != pc2)
+                {
+                    pc = pc2;
+                    break;
+                }
+            }
+            //qDebug() <<"BaseDebugger::step PC"<<pc<<i;
+            int line = -1;
+            if (i == 10) line = 0;        // It ran 10 times and get to same PC 
+            else if (flashToSource.ContainsKey(pc)) line = flashToSource[pc];
+
+            return line;
+        }
+
+        public int StepOver() { return 0; }
+
+        public int GetValidLine(int line)
+        {
+            while (!sourceToFlash.ContainsKey(line) && line <= lastLine) line++;
+            return line;
+        }
+
+        public string GetVarType(string var)
+        {
+            var = var.ToUpper();
+            return varList[var];
+        }
+
+
+        public void ProcRead()
+        {
+//            while (compProcess.CanReadLine())
+            {
+//                outPane.AppendText(QString::fromLocal8Bit(m_compProcess.readLine()));
+                outPane.AppendText("\n");
+            }
+        }
+
+        public void ReadSettings()
+        {
+//            QSettings* settings = MainWindow::self()->settings();
+
+//            if (settings->contains(m_compSetting))
+//                m_compilerPath = settings->value(m_compSetting).toString();
+        }
+
+  
+        public bool DriveCirc()
+        {
+//            CodeEditor ce = EditorWindow.Self().GetCodeEditor();
+//            return ce.DriveCirc();
+            return true; //tyv
+        }
+
+        public void SetDriveCirc(bool drive)
+        {
+//            CodeEditor ce = EditorWindow.Self().GetCodeEditor();
+//            ce.SetDriveCirc(drive);
+        }
+
+
+
+
+
         public bool DriveCircuit { get; set; }
 
-        protected TextBox outPane;
+        protected System.Windows.Controls.TextBox outPane;
 
         protected static bool loadStatus;                          // Is symbol file loaded?
         protected bool running;                             // Is processor running?
@@ -78,8 +203,8 @@ namespace SimulIDE.src.gui.editor
         protected string fileDir;
         protected string fileName;
         protected string fileExt;
+        protected int type;
 
-        
         protected string compSetting;
 
         List<string> varNames = new List<string>();
@@ -95,199 +220,8 @@ namespace SimulIDE.src.gui.editor
     }
 }
 
-//#include "mcucomponent.h"
-
-class BaseDebugger : public QObject    // Base Class for all debuggers
-{
-    Q_OBJECT
-    Q_PROPERTY(bool Drive_Circuit READ driveCirc    WRITE setDriveCirc    DESIGNABLE true USER true )
-    Q_PROPERTY(QString Compiler_Path READ compilerPath WRITE setCompilerPath DESIGNABLE true USER true )
 
 
-bool driveCirc();
-void setDriveCirc(bool drive);
-
-
-virtual void setCompilerPath(QString path);
-
-virtual bool loadFirmware();
-virtual void upload();
-virtual int step();        // Run 1 step,returns current source line number
-virtual int stepOver();    // Run until next source file
-virtual void stop();
-
-virtual void getProcName();
-virtual void setProcType(int type) { m_processorType = type; }
-
-virtual int compile()=0;
-        virtual void mapFlashToSource()=0;
-        
-        virtual int getValidLine(int line);
-
-virtual void getCompilerPath();
-
-virtual void readSettings();
-
-virtual QString getVarType(QString var);
-
-virtual QStringList getVarList();
-
-virtual QList<int> getSubLines() { return m_subLines; }
-
-int type;
-
-public slots:
-        void ProcRead();
-
-protected:
-
-};
-
-#include "basedebugger.h"
-#include "baseprocessor.h"
-#include "editorwindow.h"
-#include "mainwindow.h"
-#include "simulator.h"
-
-static const char* BaseDebugger_properties[] = {
-    QT_TRANSLATE_NOOP("App::Property","Drive Circuit"),
-    QT_TRANSLATE_NOOP("App::Property","Compiler Path")
-};
-
-bool BaseDebugger::m_loadStatus = false;
-
-BaseDebugger::BaseDebugger(QObject* parent, OutPanelText* outPane, QString filePath) 
-            : QObject(parent )
-            , m_compProcess( 0l )
-{
-    Q_UNUSED(BaseDebugger_properties);
-
-   
-}
-BaseDebugger::~BaseDebugger()
-{
-    if (BaseProcessor::self()) BaseProcessor::self()->getRamTable()->remDebugger(this);
-}
-
-bool BaseDebugger::loadFirmware()
-{
-    if (m_firmware == "") return false;
-
-    upload();
-    if (m_loadStatus) return false;
-
-    m_loadStatus = true;
-
-    return true;
-}
-
-void BaseDebugger::upload()
-{
-    if (m_loadStatus)
-    {
-        QMessageBox::warning(0, "BaseDebugger::loadFirmware",
-                                tr("Debugger already running") + "\n" + tr("Stop active session"));
-        return;
-    }
-    m_outPane->writeText("-------------------------------------------------------\n");
-    m_outPane->appendText("\n" + tr("Uploading: ") + "\n");
-    m_outPane->appendText(m_firmware);
-    m_outPane->writeText("\n\n");
-
-    if (McuComponent::self())
-    {
-        McuComponent::self()->load(m_firmware);
-        m_outPane->appendText("\n" + tr("FirmWare Uploaded to ") + McuComponent::self()->device() + "\n");
-        m_outPane->writeText("\n\n");
-
-        BaseProcessor::self()->getRamTable()->setDebugger(this);
-        mapFlashToSource();
-    }
-    else m_outPane->writeText("\n" + tr("Error: No Mcu in Simulator... ") + "\n");
-}
-
-void BaseDebugger::stop()
-{
-    m_loadStatus = false;
-}
-
-void BaseDebugger::getProcName()
-{
-}
-
-int BaseDebugger::step()
-{
-    int pc = BaseProcessor::self()->pc();
-
-    int i = 0;
-    for (i = 0; i < 10; i++) // If runs 10 times and get to same PC return 0
-    {
-        BaseProcessor::self()->stepOne();
-
-        int pc2 = BaseProcessor::self()->pc();
-        //qDebug() <<"BaseDebugger::step "<<pc<<pc2;
-        if (pc != pc2)
-        {
-            pc = pc2;
-            break;
-        }
-    }
-    //qDebug() <<"BaseDebugger::step PC"<<pc<<i;
-    int line = -1;
-    if (i == 10) line = 0;        // It ran 10 times and get to same PC 
-    else if (m_flashToSource.contains(pc)) line = m_flashToSource[pc];
-
-    return line;
-}
-
-int BaseDebugger::stepOver() { return 0; }
-
-int BaseDebugger::getValidLine(int line)
-{
-    while (!m_sourceToFlash.contains(line) && line <= m_lastLine) line++;
-    return line;
-}
-
-QString BaseDebugger::getVarType(QString var)
-{
-    var = var.toUpper();
-    return m_varList[var];
-}
-
-
-void BaseDebugger::ProcRead()
-{
-    while (m_compProcess.canReadLine())
-    {
-        m_outPane->appendText(QString::fromLocal8Bit(m_compProcess.readLine()));
-        m_outPane->writeText("\n");
-    }
-}
-
-void BaseDebugger::readSettings()
-{
-    QSettings* settings = MainWindow::self()->settings();
-
-    if (settings->contains(m_compSetting))
-        m_compilerPath = settings->value(m_compSetting).toString();
-}
-
-void BaseDebugger::getCompilerPath()
-{
-    
-}
-
-bool BaseDebugger::driveCirc()
-{
-    CodeEditor* ce = EditorWindow::self()->getCodeEditor();
-    return ce->driveCirc();
-}
-
-void BaseDebugger::setDriveCirc(bool drive)
-{
-    CodeEditor* ce = EditorWindow::self()->getCodeEditor();
-    ce->setDriveCirc(drive);
-}
 
 
 
---
+++ SimulIDE/src/simulator/elements/processors/BaseProcessor.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//# include "baseprocessor.h"
+//# include "mcucomponent.h"
+//# include "circuitwidget.h"
+//# include "mainwindow.h"
+//# include "simulator.h"
+//# include "utils.h"
+//# include "simuapi_apppath.h"
+
+
+namespace SimulIDE.src.simulator.elements.processors
+{
+    class BaseProcessor
+    {
+
+
+        protected static BaseProcessor self = null;
+
+        public BaseProcessor(object parent ):base()
+        {
+            loadStatus = false;
+            resetStatus = false;
+            symbolFile = "";
+            device = "";
+
+            //ramTable = new RamTable(this);
+            //MainWindow::self()->m_ramTabWidgetLayout->addWidget(m_ramTable);
+        }
+
+        public void Terminate()
+        {
+            //qDebug() <<"\nBaseProcessor::terminate "<<m_device<<m_symbolFile<<"\n";
+
+            self = null;
+            m_loadStatus = false;
+            symbolFile = "";
+        }
+
+        public void Initialized()
+        {
+            //qDebug() << "\nBaseProcessor::initialized  Firmware: " << m_symbolFile;
+            //qDebug() << "\nBaseProcessor::initialized Data File: " << m_dataFile;
+
+            loadStatus = true;
+            msimStep = 0;
+            extraCycle = 0;
+        }
+
+        public void SetExtraStep() // Run Extra Simulation Step If MCU clock speed > Simulation speed
+        {
+            if (mcuStepsPT > 1) extraCycle = Cycle();
+        }
+
+        public void Step()
+        {
+            if (!loadStatus || resetStatus) return;
+
+            while (nextCycle >= 1)
+            {
+                StepCpu();
+                nextCycle -= 1;
+
+                if (extraCycle)
+                {
+                    //qDebug() << "-" << m_extraCycle;qDebug() << " ";
+                    Simulator.Self().RunExtraStep(extraCycle);
+                    extraCycle = 0;
+                }
+            }
+            nextCycle += mcuStepsPT;
+        }
+
+        public void RunSimuStep()
+        {
+            Simulator.Self().RunCircuit();
+
+            msimStep++;
+            if (msimStep >= 50000 * Simulator.Self().stepsPerus()) // 20 fps
+            {
+                msimStep = 0;
+                Simulator.Self().RunGraphicStep2();
+            }
+        }
+
+        public void SetSteps(double steps) { mcuStepsPT = steps; }
+
+        public string GetFileName() { return symbolFile; }
+
+        public void SetDevice(string device) { this.device = device; }
+
+        public string GetDevice() { return device; }
+
+        public void SetDataFile(string datafile)
+        {
+            dataFile = datafile;
+            SetRegisters();
+        }
+
+        public void HardReset(bool rst)
+        {
+            resetStatus = rst;
+            if (rst) McuComponent.Self()->Reset();
+        }
+
+        public int GetRegAddress(string name)
+        {
+            name = name.ToUpper();
+            if (regsTable.ContainsKey(name)) return regsTable.value(name);
+            return -1;
+        }
+
+        void BaseProcessor::updateRamValue(QString name)
+        {
+            if (!m_loadStatus) return;
+
+            name = name.toUpper();
+            QString type = "";
+            if (m_typeTable.contains(name)) type = m_typeTable[name];
+            else return;
+
+            QByteArray ba;
+            ba.resize(4);
+            int address = getRegAddress(name);
+            if (address < 0) return;
+
+            int bits = 8;
+
+            if (type.contains("32"))    // 4 bytes
+            {
+                bits = 32;
+                ba[0] = getRamValue(address);
+                ba[1] = getRamValue(address + 1);
+                ba[2] = getRamValue(address + 2);
+                ba[3] = getRamValue(address + 3);
+            }
+            else if (type.contains("16"))  // 2 bytes
+            {
+                bits = 16;
+                ba[0] = getRamValue(address);
+                ba[1] = getRamValue(address + 1);
+                ba[2] = 0;
+                ba[3] = 0;
+            }
+            else                                  // 1 byte
+            {
+                ba[0] = getRamValue(address);
+                ba[1] = 0;
+                ba[2] = 0;
+                ba[3] = 0;
+            }
+            if (type.contains("f"))                          // float, double
+            {
+                float value = 0;
+                memcpy(&value, ba, 4);
+                m_ramTable->setItemValue(1, value);
+            }
+            else                                              // char, int, long
+            {
+                int32_t value = 0;
+
+                if (type.contains("u"))
+                {
+                    uint32_t val = 0;
+                    memcpy(&val, ba, 4);
+                    value = val;
+                }
+                else
+                {
+                    if (bits == 32)
+                    {
+                        int32_t val = 0;
+                        memcpy(&val, ba, 4);
+
+                        value = val;
+                    }
+                    else if (bits == 16)
+                    {
+                        int16_t val = 0;
+                        memcpy(&val, ba, 2);
+
+                        value = val;
+                    }
+                    else
+                    {
+                        int8_t val = 0;
+                        memcpy(&val, ba, 1);
+
+                        value = val;
+                    }
+                }
+                m_ramTable->setItemValue(2, value);
+
+                if (type.contains("8")) m_ramTable->setItemValue(3, decToBase(value, 2, 8));
+                else if (type.contains("string"))
+                {
+                    QString strVal = "";
+                    for (int i = address; i <= address + value; i++)
+                    {
+                        QString str = "";
+                        const QChar cha = getRamValue(i);
+                        str.setRawData(&cha, 1);
+
+                        strVal += str; //QByteArray::fromHex( getRamValue( i ) );
+                    }
+                    //qDebug() << "string" << name << value << strVal;
+                    m_ramTable->setItemValue(3, strVal);
+                }
+
+            }
+            //qDebug()<<name<<type <<address<<value;
+            //if( !type.contains( "8" ) ) 
+            m_ramTable->setItemValue(1, type);
+        }
+
+
+        int BaseProcessor::getRamValue(QString name)
+        {
+            if (m_regsTable.isEmpty()) return -1;
+
+            bool isNumber = false;
+            int address = name.toInt(&isNumber);      // Try to convert to integer
+
+            if (!isNumber)
+            {
+                address = m_regsTable[name.toUpper()];  // Is a register name
+            }
+
+            return getRamValue(address);
+        }
+
+        void BaseProcessor::addWatchVar(QString name, int address, QString type)
+        {
+            name = name.toUpper();
+            if (!m_regsTable.contains(name)) m_regList.append(name);
+            m_regsTable[name] = address;
+            m_typeTable[name] = type;
+        }
+
+        void BaseProcessor::setRegisters() // get register addresses from data file
+        {
+            QStringList lineList = fileToStringList(m_dataFile, "BaseProcessor::setRegisters");
+
+            if (!m_regsTable.isEmpty())
+            {
+                m_regList.clear();
+                m_regsTable.clear();
+                m_typeTable.clear();
+            }
+
+            for (QString line : lineList)
+            {
+                if (line.contains("EQU "))   // This line contains a definition
+                {
+                    line = line.replace("\t", " ");
+
+                    QString name = "";
+                    QString addrtxt = "";
+                    int address = 0;
+                    bool isNumber = false;
+
+                    line.remove(" ");
+                    QStringList wordList = line.split("EQU"); // Split in words
+                    if (wordList.size() < 2) continue;
+
+                    name = wordList.takeFirst();
+                    while (addrtxt.isEmpty()) addrtxt = wordList.takeFirst();
+
+                    address = addrtxt.toInt(&isNumber, 10);
+
+                    if (isNumber)        // If found a valid address add to map
+                    {
+                        address = validate(address);
+                        addWatchVar(name, address, "u8");        // type uint8 
+                    }
+                    //qDebug() << name << address<<"\n";
+                }
+            }
+        }
+
+        void BaseProcessor::uartOut(int uart, uint32_t value) // Send value to OutPanelText
+        {
+            emit uartDataOut(uart, value );
+
+            //qDebug()<<"BaseProcessor::uartOut" << uart << value;
+        }
+
+        void BaseProcessor::uartIn(int uart, uint32_t value) // Receive one byte on Uart
+        {
+            //qDebug()<<"BaseProcessor::uartIn" << uart << value;
+            emit uartDataIn(uart, value );
+        }
+
+
+        protected virtual int Validate(int address) { return 0; }
+
+        protected void RunSimuStep() { }
+
+        protected string symbolFile;
+        protected string dataFile;
+        protected string device;
+
+        protected double nextCycle;
+        protected double mcuStepsPT;
+        protected int msimStep;
+        protected UInt64 m_extraCycle;
+
+        protected RamTable ramTable;
+        protected List<string> regList=new List<string>();
+        protected Dictionary<string, int> regsTable = new Dictionary<string, int>();     // int max 32 bits
+        protected Dictionary<string, float> floatTable = new Dictionary<string, float>();  // float 32 bits
+        protected Dictionary<string, string> typeTable = new Dictionary<string, string>();
+
+        protected List<int> eeprom = new List<int>();
+
+        protected bool m_resetStatus;
+        protected bool m_loadStatus;
+
+    }
+}
+
+
+
