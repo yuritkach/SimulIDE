d648652b57a1ed9d1efc45108ce791ea9b01da57
yuritkach <yuritkach@gmail.com>
2020-12-02 23:42:52 +0200

    fff

---
+++ Test/Command.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avr
+{
+    public abstract class BaseCommand
+    {
+        private readonly ALU alu;
+        protected BaseCommand(ALU alu)
+        {
+            this.alu = alu;
+        }
+        public abstract bool ItsMe(ushort word);
+        public abstract void Execute();
+        public abstract string Disasemble();
+    }
+
+    public class ADCCommand : BaseCommand
+    {
+        ADCCommand(ALU alu):base(alu){}
+
+        public override string Disasemble()
+        {
+            return "ADC R1,R2"; // R1-R2 нужно привести к реальным названиями (по младшим битам команды)
+        }
+
+        public override void Execute()
+        {
+            // Нужно:
+            // 1. расчитать результат и положить его в регистр (OnChanged)
+            // 2. Увеличить PC (OnChanged)
+            // 3. Изменить состояние SREG (через свойство, чтоб послать event OnChanged)
+            // 4. Увеличить значение счетчика Clock (OnChanged)
+        }
+
+        public override bool ItsMe(ushort command)
+        {
+            return (command & 0b0001110000000000) == 0b0001110000000000;
+        }
+    }
+    
+
+}
---
+++ Test/Core.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avr
+{
+    [Flags]
+    public enum StatusRegister
+    {
+        C = 1,
+        Z = 2,
+        N = 4,
+        V = 8,
+        S = 16,
+        H = 32,
+        T = 64,
+        I = 128
+    }
+
+    public class FlashProgrammMemory
+    {
+        public FlashProgrammMemory()
+        {
+            data = new byte[0x3FFF];
+        }
+
+        public static byte[] GetData(UInt16 address)
+        {
+            return new byte[2] { data[address], data[address + 1] };
+        }
+
+        protected static byte[] data;
+    }
+
+    public class ALU
+    {
+        public ALU()
+        {
+            PC = 0;
+            ClockCounter = 0;
+        }
+
+        public void DoClock()
+        {
+            GetCommand();
+            ExecuteCommand();
+            ClockCounter++;
+        }
+
+        protected void GetCommand()
+        {
+            byte[] command = FlashProgrammMemory.GetData(PC);
+        }
+
+        protected void ExecuteCommand()
+        {
+            byte[] command = FlashProgrammMemory.GetData(PC);
+        }
+
+
+        protected UInt16 PC;
+        protected UInt64 ClockCounter;
+        protected StatusRegister SREG;
+    }
+
+
+}
